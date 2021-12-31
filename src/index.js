0f90fda27299a285f300f5a757e37a4cd1581fc6
Toney Tate <tdatate@gmail.com>
2021-12-31 14:08:52 -0500

    Add Previous entries function for save button.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el");
 let count = 0
 
 function increment() {
-    count = count + 1
+    count += 1
     countEl.innerText = count
 }
 
-// 1. Create a function, save(), which logs out the count when it's called
 function save() {
-    console.log(count)
+    let saveVar = " " + count + " -"
+    saveEl.innerText += saveVar;
+    console.log(count);
 }
 
 
